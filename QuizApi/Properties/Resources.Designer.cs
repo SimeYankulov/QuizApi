8af37ba676b2514419ad51fe12ceb1b2a52ab756
sime jankulov <jankulov.sime@yahoo.com>
2023-01-30 18:58:43 +0200

    Renamed the project

--- QuizApi/Properties/Resources.Designer.cs
+++ QuizApi/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace QuizApi.Properties {
+namespace UsersApi.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace QuizApi.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("QuizApi.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("UsersApi.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
